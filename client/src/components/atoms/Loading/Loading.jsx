9a76f526a2e4d7cb4a8f6a449f9a8ef37989dfda
Manuel Zavala <majezanu@gmail.com>
2020-03-15 10:34:59 -0600

    fix: add className to loading component

--- client/src/components/atoms/Loading/Loading.jsx
+++ client/src/components/atoms/Loading/Loading.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Loading = (props) => {    
     return (
         <div className={`spinner-grow ${props.style ? props.tyle : 'text-primary'}`} role="status">
-            <span class="sr-only">Loading...</span>
+            <span className="sr-only">Loading...</span>
         </div>
     )
 };
