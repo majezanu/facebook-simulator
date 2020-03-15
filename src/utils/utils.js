915089b6a87795d291f65a29daf198c0b5c64d60
Manuel Zavala <majezanu@gmail.com>
2020-03-15 16:08:06 -0600

    fix: increment expiration time

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const SALT_ROUNDS = 12;
-const TOKEN_EXPIRATION = 60;
+const TOKEN_EXPIRATION = 3600;
 const {TOKEN_SECRET} = process.env;
 
 const matchHash = (plainPassword, hashedPassword) => bcrypt.compareSync(plainPassword, hashedPassword);
